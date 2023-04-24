0bf30a0d5088f0395b52b0487dc94a1f19fdf62c
Otzzu <mesachharmasendro@gmail.com>
2023-04-24 19:16:35 +0700

    melengkapi komentar

--- B04_Undo.py
+++ B04_Undo.py
@@ -12,9 +12,9 @@ def undo() -> None:
         print("Undo hanya dapat diakses oleh akun Bandung Bondowoso.")
     else:
         if var.stackUndo[1] >= 1: # pengecekan apakah ada jin yang bisa dibangkitkan kembali
-            last = getLast()
-            jin = last[0]
-            candis = last[1]
+            last = getLast() # (undoData)
+            jin = last[0] # (user)
+            candis = last[1] # (arrCandi)
             if filterArr(var.users, lambda x: x[0] == jin[0])[1] != 0 or var.users[1] >= 102:# pengecekan apakah jumlah jin sudah >= 100 atau belum dan pengecekan apakah username jin yang mau dibangkitkan sudah ada atau belum
                 print("Undo gagal")
             else:
@@ -24,9 +24,9 @@ def undo() -> None:
                     # pembangunan ulang candi
                     for i in range(candis[1]):
                         id = generateIdCandi()
-                        candi = (id, candis[0][i][1], candis[0][i]
+                        candiTemp = (id, candis[0][i][1], candis[0][i]
                                  [2], candis[0][i][3], candis[0][i][4])
-                        var.candi = add(candi, var.candi)
+                        var.candi = add(candiTemp, var.candi)
                 print("Undo berhasil")
         else:
             print("Tidak ada yang bisa di undo")
