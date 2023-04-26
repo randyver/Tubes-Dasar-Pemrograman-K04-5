3bac21289186bca0cb73f7bd2cf1a60d2f2f09d9
keziachyd <131640065+keziachyd@users.noreply.github.com>
2023-04-26 13:40:55 +0700

    Create F12_Ayam Berkokok.py

---
+++ F12_Ayam Berkokok.py
@@ -0,0 +1,23 @@
+import var
+from util import *
+from typing import *
+import os
+
+def AyamBerkokok(jumlah_candi):
+  if role!= "roro_jonggrang" :
+    print("Anda tidak bisa mengakses program ini")
+    else :
+      print("Kukuruyuk.. Kukuruyuk..")
+      jumlah_candi = 
+      
+      
+      if jumlah_candi >= 100:
+          print("Yah, Bandung Bondowoso memenangkan permainan!")
+      else:
+          print("Selamat, Roro Jonggrang memenangkan permainan!")
+          print("*Bandung Bondowoso angry noise*")
+          print("Roro Jonggrang dikutuk menjadi candi")
+        
+# Contoh penggunaan fungsi AyamBerkokok
+AyamBerkokok(75) # Output: Roro Jonggrang menang!
+AyamBerkokok(150) # Output: Bandung Bondowoso menang!
